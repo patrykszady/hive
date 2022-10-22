00c039d63ddf1a565bf68c0815d62fb48cfc4f5c
vagrant <vagrant@homestead>
2022-10-22 22:44:41 +0000

    add AMOUNT to Checks

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,16 +24,25 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // only in Production not in Development enviroment ... ANYTHING EMAIL RELATED GOES HERE
-        if(env('APP_ENV') == 'production'){
-            $schedule->call('\App\Http\Controllers\ReceiptController@receipt_email')->everyMinute();  
+        // only in Production not in Development enviroment ... EVERYTHING EMAIL RELATED GOES HERE
+        // if(env('APP_ENV') == 'production'){
+        //     $schedule->call('\App\Http\Controllers\ReceiptController@receipt_email')->everyMinute();  
             
-            $schedule->call('\App\Http\Controllers\TransactionController@plaid_transactions_scheduled')->hourly();
-            $schedule->call('\App\Http\Controllers\TransactionController@add_check_deposit_to_transactions')->everyTenMinutes();
-            $schedule->call('\App\Http\Controllers\TransactionController@add_vendor_to_transactions')->everyTenMinutes();
-            $schedule->call('\App\Http\Controllers\TransactionController@add_check_id_to_transactions')->everyTenMinutes(); 
-            $schedule->call('\App\Http\Controllers\TransactionController@add_expense_to_transactions')->everyTenMinutes();            
-        }
+        //     $schedule->call('\App\Http\Controllers\TransactionController@plaid_transactions_scheduled')->hourly();
+        //     $schedule->call('\App\Http\Controllers\TransactionController@add_check_deposit_to_transactions')->everyTenMinutes();
+        //     $schedule->call('\App\Http\Controllers\TransactionController@add_vendor_to_transactions')->everyTenMinutes();
+        //     $schedule->call('\App\Http\Controllers\TransactionController@add_check_id_to_transactions')->everyTenMinutes(); 
+        //     $schedule->call('\App\Http\Controllers\TransactionController@add_expense_to_transactions')->everyTenMinutes();            
+        // }
+
+        $schedule->call('\App\Http\Controllers\ReceiptController@receipt_email')->everyMinute();  
+            
+        $schedule->call('\App\Http\Controllers\TransactionController@plaid_transactions_scheduled')->hourly();
+        $schedule->call('\App\Http\Controllers\TransactionController@add_check_deposit_to_transactions')->hourly();
+        $schedule->call('\App\Http\Controllers\TransactionController@add_vendor_to_transactions')->hourly();
+        $schedule->call('\App\Http\Controllers\TransactionController@add_check_id_to_transactions')->hourly(); 
+        $schedule->call('\App\Http\Controllers\TransactionController@add_expense_to_transactions')->hourly();      
+
         // $schedule->call('\App\Http\Controllers\ReceiptController@receipt_email')->everyMinute();
 
         //Transactions bidaily/hourly
