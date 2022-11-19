cf6cfea16d80943d30fb98d24e3fbca36bb86a58
vagrant <vagrant@homestead>
2022-11-19 04:05:20 +0000

    find_credit_payments_on_debit 2

--- resources/views/livewire/expenses/show.blade.php
+++ resources/views/livewire/expenses/show.blade.php
@@ -160,7 +160,7 @@
 
 	<div class="col-span-4 lg:col-span-2 space-y-2">
 		{{-- ASSOCIATED EXPENSES --}}
-		@if(!$expense->associated_expenses->isEmpty())
+		@if(!is_null($expense->associated_expenses))
 		<x-cards.wrapper class="col-span-4 lg:col-span-2 lg:col-start-3">
 			<x-cards.heading>
 				<x-slot name="left">
@@ -186,7 +186,7 @@
 								],
 							];
 					@endphp
-{{-- 
+					{{-- 
 					@if($split->reimbursment == 'Client')
 						@php
 							$line_details[] = [
